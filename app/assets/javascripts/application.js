645f82dba7fc617cd2a93148e0d6b22421c7bb19
Reza <rezazramadhan@gmail.com>
2018-10-30 14:08:22 +0700

    add modal for create todolist

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -5,3 +5,23 @@ function fullscreen() {
     var heights = window.innerHeight - 72.8;
     document.getElementById("home").style.height = heights + "px";
 }
+
+var todolistModal = document.getElementById('todolistModal');
+
+var todolistBtn = document.getElementById("todolistBtn");
+
+var todolistSpan = document.getElementsByClassName("close")[0];
+
+todolistBtn.onclick = function() {
+    todolistModal.style.display = "block";
+}
+
+todolistSpan.onclick = function() {
+    todolistModal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == todolistModal) {
+        todolistModal.style.display = "none";
+    }
+}
